d716f2bf1048209586a8a081be247b25cded08f8
Sohail Nazir Shaikh <sohailwciplclt20@gmail.com>
2024-03-19 17:34:34 +0530

    updated for test

--- db.js
+++ db.js
@@ -3,21 +3,30 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const db = mysql.createPool({
+const db = mysql.createConnection({
     host: process.env.DbHost,
     user: process.env.DbUser,
     password: process.env.DbPassword,
     database: process.env.DbName,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+    waitForConnections: true
+    // connectionLimit: 10,
+    // queueLimit: 0
 });
 
-db.getConnection((err) => {
+db.connect((err) => {
     if(err)
         console.error('Error connecting to MySQL Database', err);
     else
         console.log('Successfully connected to MySQL Database');
 });
+
+db.end((err) => {
+    if(err)
+    {
+        console.log(err);
+        return;
+    }
+    console.log('connection closed');
+})
 
 module.exports = {db};
